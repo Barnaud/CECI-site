210c9c1f4e1233bb59f508d91f866513f988d0b3
root <root@sd-130010.dedibox.fr>
2019-10-15 22:21:47 +0200

    add mail_cron

--- mail_cron.py
+++ mail_cron.py
@@ -3,7 +3,8 @@ from monApp import models
 from monApp.util import endHour
 import datetime
 
-mails_to_send = models.PlannedMail.objects.filter(time__lte = endHour(datetime.datetime.now()))
+mails_to_send = models.PlannedMail.objects.filter(time__lte = endHour(datetime.datetime.now())+datetime.timedelta(weeks=2))
+print(mails_to_send)
 
 for mail in mails_to_send:
 	mail.send()
